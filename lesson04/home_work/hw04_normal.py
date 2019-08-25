ee50403796db06f4e36c7666066a9e4a009d66ab
Gasby <4226793@gmail.com>
2019-08-25 11:53:50 +0300

    no message

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -22,6 +22,7 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 
 
 # Задание-2:
+
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
 # Т.е. из строки 
@@ -44,7 +45,42 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+symbol1 = list(map(lambda x: chr(x), list(range(65, 91))))
+symbol2 = list(map(lambda x: chr(x), list(range(97, 123))))
+line_new = list(line_2)
 
+lst = []
+i = len(line_new) - 1
+
+while i >= 0:
+       if line_new[i] in symbol2:
+              lst.append(line_new[i])
+       elif line_new[i] in symbol1 and i <= len(line_new) - 3 and line_new[i + 1] in symbol1 and line_new[
+              i + 2] in symbol1:
+              lst.append(line_new[i])
+       else:
+              lst.append(' ')
+       i -= 1
+lst.reverse()
+
+i = 0
+lst2 = []
+registr = True
+while i <= len(lst) - 1:
+       if lst[i] in symbol2:
+              registr = True
+       if lst[i] in symbol1 and lst[i - 1] in symbol2 and lst[i - 2] in symbol2:
+              lst2.append(lst[i])
+              registr = False
+       elif lst[i] in symbol1 and registr == False:
+              lst2.append(lst[i])
+       else:
+              lst2.append(' ')
+       i += 1
+stroka = ''.join(lst2).split(' ')
+
+line_str_3 = [i for i in stroka if i != '']
+print('Список без использованием модуля re: \n', line_str_3)
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
